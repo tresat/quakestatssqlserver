2688b71ea33551f1415e98882754c881152c3db5
tresat <tom.tresansky@gmail.com>
2009-06-02 23:49:11 -0400

    -Added My Project\ folders to git tracking, since needed to build solution.

---
+++ QuakeStats/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("QuakeStats")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Anon Inc.")> 
+<Assembly: AssemblyProduct("QuakeStats")> 
+<Assembly: AssemblyCopyright("Copyright © Anon Inc. 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("799f38ab-28f3-409a-90db-eecc8e7e5239")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ QuakeStatsUnitTests/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("QuakeStatsUnitTests")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Anon Inc.")> 
+<Assembly: AssemblyProduct("QuakeStatsUnitTests")> 
+<Assembly: AssemblyCopyright("Copyright © Anon Inc. 2009")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("38da2d1a-1ae7-4e53-af8d-886abbfac6d3")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
